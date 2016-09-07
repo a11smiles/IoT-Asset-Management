36b1dd422712123ef0a0849b09cf4cdb518c8b21
Joshua Davis <me@jdav.is>
2016-09-07 14:24:40 -0400

    added routes for individual asset

--- webapp/app/routes/api/asset.js
+++ webapp/app/routes/api/asset.js
@@ -5,10 +5,10 @@ module.exports = function (app, express) {
 
     // /api/assets
     assetRouter.route('/')
-        
+
         // create an asset
-        .post(function(req, res) {
-            
+        .post(function (req, res) {
+
             // create new instance of asset and hydrate properties from request object
             var asset = new Asset();
             asset.id = req.body.id;
@@ -16,10 +16,15 @@ module.exports = function (app, express) {
             asset.owner = req.body.owner;
 
             // save asset
-            asset.save(function(err) {
-                if (err) res.send(err);
+            asset.save(function (err) {
+                if (err) {
+                    if (err.code == 11000)
+                        return res.json({ success: false, message: 'Asset with that Id already exists.' });
+                    else
+                        res.send(err);
+                }
 
-                res.json({ message: 'Asset saved.'});
+                res.json({ message: 'Asset saved.' });
             });
         })
 
@@ -34,7 +39,46 @@ module.exports = function (app, express) {
             });
         });
 
-    
+    // /api/assets/:id
+    assetRouter.route('/:id')
+
+        // get an asset
+        .get(function (req, res) {
+            Asset.findOne({ 'id' : req.params.id }, function(err, asset) {
+                if (err) res.send(err);
+
+                // return asset
+                res.json(asset);
+            });
+        })
+
+        // update an asset
+        .put(function (req, res) {
+            Asset.findOne({ 'id' : req.params.id }, function(err, asset) {
+                if (err) res.send(err);
+
+                // update asset by hydrating properties from request object
+                asset.description = req.body.description;
+                asset.owner = req.body.owner;
+                lastUpdate = new Date().toISOString();
+                
+                // save asset
+                asset.save(function (err) {
+                    if (err) res.send(err);
+
+                    res.json({ message: 'Asset updated.' });
+                });
+            });
+        })
+
+        // delete an asset
+        .delete(function (req, res) {
+            Asset.remove({ 'id' : req.params.id }, function(err, asset) {
+                if (err) res.send(err);
+
+                res.json({ message: 'Asset deleted successfully. '});
+            });
+        });
 
     return assetRouter;
 }
