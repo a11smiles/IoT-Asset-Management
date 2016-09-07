9397be4a820d50b8589b48f2e97540f13a33237f
Joshua Davis <me@jdav.is>
2016-09-06 21:13:24 -0400

    created asset schema

---
+++ webapp/app/models/asset.js
@@ -0,0 +1,16 @@
+// community libs
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+// asset model
+var AssetSchema = new Schema({
+    id: { type: String, required: true, index: { unique: true } },
+    description: { type: String, required: true },
+    owner: { type: String, required: false },
+    lastUpdate: { type: Date, default: Date.now, required: true },
+    lastLocation: { type: String, required: false }
+});
+
+// return the model
+module.exports = mongoose.model('Asset', AssetSchema);
