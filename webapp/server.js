624da9236750a2395bffb8f7de2a1913a6ede0c6
Joshua Davis <me@jdav.is>
2016-09-07 11:35:33 -0400

    added bodyparser and CORS settings

--- webapp/server.js
+++ webapp/server.js
@@ -1,7 +1,8 @@
 // community libs
 var express = require('express'),
     morgan = require('morgan'),
-    path = require('path');
+    path = require('path'),
+    bodyParser = require('body-parser');
 
 // custom libs
 var config = require('./config.js');
@@ -9,11 +10,23 @@ var config = require('./config.js');
 // instantiate server
 var app = express();
 
-// configure public assets folder
-app.use(express.static(__dirname + '/public'));
+// use body parser to grab information from POST requests
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+
+// configure to handle CORS requests
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+    next();
+});
 
 // turn on logging
 app.use(morgan('dev'));
+
+// configure public assets folder
+app.use(express.static(__dirname + '/public'));
 
 // add routes
 var apiRoutes = require('./app/routes/api/index')(app, express);
