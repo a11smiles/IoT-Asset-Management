2c08d674e84f854ce608725852c482cb1592be96
Joshua Davis <me@jdav.is>
2016-09-06 22:09:34 -0400

    adding the GET and POSt routes for /api/assets

--- webapp/app/routes/api/asset.js
+++ webapp/app/routes/api/asset.js
@@ -3,14 +3,38 @@ var Asset = require('../../models/asset.js');
 module.exports = function (app, express) {
     var assetRouter = express.Router();
 
+    // /api/assets
     assetRouter.route('/')
+        
+        // create an asset
+        .post(function(req, res) {
+            
+            // create new instance of asset and hydrate properties from request object
+            var asset = new Asset();
+            asset.id = req.body.id;
+            asset.description = req.body.description;
+            asset.owner = req.body.owner;
+
+            // save asset
+            asset.save(function(err) {
+                if (err) res.send(err);
+
+                res.json({ message: 'Asset saved.'});
+            });
+        })
+
+        // return a list of assets
         .get(function (req, res) {
+
+            // query database for all assets
             Asset.find(function (err, assets) {
                 if (err) res.send(err);
 
                 res.json(assets);
             });
         });
+
+    
 
     return assetRouter;
 }
