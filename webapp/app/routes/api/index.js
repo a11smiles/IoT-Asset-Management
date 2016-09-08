8c6991e66abbc806d59e6c51789a3e648d35a5d7
Joshua Davis <me@jdav.is>
2016-09-07 20:54:28 -0400

    added user api routes

--- webapp/app/routes/api/index.js
+++ webapp/app/routes/api/index.js
@@ -9,10 +9,11 @@ module.exports = function(app, express) {
 
     // add routes for assets api
     var AssetRoutes = require('./asset.js')(app, express);
-    apiRouter.use("/assets", AssetRoutes);
+    apiRouter.use('/assets', AssetRoutes);
 
     // add routes for users api
-    
+    var UserRoutes = require('./user.js')(app, express);
+    apiRouter.use('/users', UserRoutes);
 
     return apiRouter;
 }
