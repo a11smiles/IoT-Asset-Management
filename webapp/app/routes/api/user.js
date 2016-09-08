254aab5e25a1eb1a5ae2f6e029f49b4d9dd9220b
Joshua Davis <me@jdav.is>
2016-09-07 20:55:23 -0400

    added user routes

---
+++ webapp/app/routes/api/user.js
@@ -0,0 +1,81 @@
+var User = require('../../models/user.js');
+
+module.exports = function (app, express) {
+    var userRouter = express.Router();
+
+    // /api/users
+    userRouter.route('/')
+
+        // create a user
+        .post(function (req, res) {
+
+            // create new instance of user and hydrate properties from request object
+            var user = new User();
+            user.username = req.body.username;
+            user.password = req.body.password;
+
+            // save user
+            user.save(function (err) {
+                if (err) {
+                    if (err.code == 11000)
+                        return res.json({ success: false, message: 'A user with that username already exists.' });
+                    else
+                        res.send(err);
+                }
+
+                res.json({ message: 'User saved.' });
+            });
+        })
+
+        // return a list of users
+        .get(function (req, res) {
+
+            // query database for all users
+            User.find(function (err, users) {
+                if (err) res.send(err);
+
+                res.json(users);
+            });
+        });
+
+    // /api/users/:username
+    userRouter.route('/:username')
+
+        // get a user
+        .get(function (req, res) {
+            User.findOne({ 'username' : req.params.username }, function(err, user) {
+                if (err) res.send(err);
+
+                // return user
+                res.json(user);
+            });
+        })
+
+        // update a user
+        .put(function (req, res) {
+            User.findOne({ 'username' : req.params.username }, function(err, user) {
+                if (err) res.send(err);
+
+                // update user by hydrating properties from request object
+                user.password = req.body.password;
+                
+                // save user
+                user.save(function (err) {
+                    if (err) res.send(err);
+
+                    res.json({ message: 'User updated.' });
+                });
+            });
+        })
+
+        // delete a user
+        .delete(function (req, res) {
+            User.remove({ 'username' : req.params.username }, function(err, username) {
+                if (err) res.send(err);
+
+                res.json({ message: 'User deleted successfully. '});
+            });
+        });
+
+    return userRouter;
+}
