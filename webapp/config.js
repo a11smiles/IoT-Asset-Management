6c8a7d6094c54a3ddaa57bdfbf0006a247896748
Joshua Davis <me@jdav.is>
2016-09-07 20:53:41 -0400

    added jwt token secret

--- webapp/config.js
+++ webapp/config.js
@@ -1,7 +1,7 @@
 module.exports = {
  'port': process.env.PORT || 8080,
- 'repo' : 'documentdb',
+ 'repo' : 'mongodb',
  'mongodb': 'mongodb://testadmin:testpassword@ds019946.mlab.com:19946/iot-assets',
  'documentdb' : 'mongodb://iot-assets:qpYJzAGW2Ahzcr82YViwYkEjwr5Kat31jtfyOESYZYIdOvlbUelEgR0E8tdlUkUgPbALIfq2uoQ4R5Z1cmrUhA==@iot-assets.documents.azure.com:10250/?ssl=true',
- 'secret' : ''
+ 'secret' : 'myJWTSuperSecret'
 }
